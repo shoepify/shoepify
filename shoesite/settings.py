aa2a4aecd465128f17963d459e9eb2f646f54caa
Rivar Kaya <rivarkaya@sabanciuniv.edu>
2024-11-13 19:51:30 +0300

    basic user login

--- shoesite/settings.py
+++ shoesite/settings.py
@@ -11,6 +11,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import mysql.connector
 import MySQLdb
@@ -47,8 +48,29 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shoesite',
     'django_celery_beat',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -80,6 +102,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'shoesite.wsgi.application'
+
 
 
 # Database
